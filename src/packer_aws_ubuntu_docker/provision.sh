7357019b9fc6776c97a6842e78c7ff88d7078d59
Nick Ramirez <nickram44@gmail.com>
2017-11-15 22:36:19 -0500

    Removed 'sudo' from docker commands in gitlab-ci.yml. Downloaded docker binaries to packer image.

--- src/packer_aws_ubuntu_docker/provision.sh
+++ src/packer_aws_ubuntu_docker/provision.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 if [ ! $(which docker) ]; then
+    echo "Installing docker from apt"
     sudo apt update
     sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+    echo "Installing docker-compose"
     sudo apt update
     sudo apt install -y docker-ce
     sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+
+    echo "Downloading Docker binaries"
+    curl https://download.docker.com/linux/static/stable/x86_64/docker-17.09.0-ce.tgz | sudo tar -xvz -C /opt
 fi
